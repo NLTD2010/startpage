3e1f5c413be7387ef6ab7d857976897e96fef552
NLTD2010 <neverhelpyou@gmail.com>
2022-02-22 16:16:43 +0700

    simple bento 😑

---
+++ config.js
@@ -0,0 +1,224 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Thái Dương',
+  imageBackground: true,
+  openInNewTab: true,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: 'Good morning!',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Go to Sleep!',
+
+  // Layout
+  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+  // Weather
+  weatherKey: '4d8fb5b93d4af21d66a2948710284366', // Write here your API Key
+  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'vi', // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '16.036240',
+  defaultLongitude: '108.190650',
+
+  // Autochange
+  autoChangeTheme: true,
+
+  // Autochabge by OS
+  changeThemeByOS: false,
+
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: true,
+  hourDarkThemeActive: '17:00',
+  hourDarkThemeInactive: '06:00',
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: '0',
+      name: 'Youtube',
+      icon: 'youtube',
+      link: 'https://youtube.com',
+    },
+    {
+      id: '1',
+      name: 'Github',
+      icon: 'github',
+      link: 'https://github.com/',
+    },
+    {
+      id: '3',
+      name: 'Facebook',
+      icon: 'facebook',
+      link: 'https://facebook.com',
+    },
+    {
+      id: '4',
+      name: 'Codepen',
+      icon: 'codepen',
+      link: 'https://codepen.io',
+    },
+    {
+      id: '5',
+      name: 'Opencore',
+      icon: 'clover',
+      link: 'https://dortania.github.io/OpenCore-Install-Guide',
+    },
+  ],
+
+  secondButtonsContainer: [
+    {
+      id: '1',
+      name: 'Music',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '2',
+      name: 'twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '3',
+      name: 'bot',
+      icon: 'bot',
+      link: 'https://discord.com/app',
+    },
+    {
+      id: '4',
+      name: 'Amazon',
+      icon: 'shopping-bag',
+      link: 'https://amazon.com/',
+    },
+    {
+      id: '5',
+      name: 'Hashnode',
+      icon: 'pen-tool',
+      link: 'https://hashnode.com/',
+    },
+    {
+      id: '6',
+      name: 'Figma',
+      icon: 'figma',
+      link: 'https://figma.com/',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
+  firstlistsContainer: [
+    {
+      icon: 'music',
+      id: '1',
+      links: [
+        {
+          name: 'Music',
+          link: 'https://www.youtube.com/watch?v=P0t7y9n8xf8&list=RDMM&index=2',
+        },
+        {
+          name: 'Classic',
+          link: 'https://www.youtube.com/watch?v=P0t7y9n8xf8&list=RDMM&index=2',
+        },
+        {
+          name: 'Oldies',
+          link: 'https://www.youtube.com/watch?v=P0t7y9n8xf8&list=RDMM&index=2',
+        },
+        {
+          name: 'Rock',
+          link: 'https://www.youtube.com/watch?v=P0t7y9n8xf8&list=RDMM&index=2',
+        },
+      ],
+    },
+    {
+      icon: 'coffee',
+      id: '2',
+      links: [
+        {
+          name: 'Linkedin',
+          link: 'https://www.linkedin.com',
+        },
+        {
+          name: 'Dribbble',
+          link: 'https://www.dribbble.com',
+        },
+        {
+          name: 'Trello',
+          link: 'https://www.trello.com',
+        },
+        {
+          name: 'Slack',
+          link: 'https://www.slack.com',
+        },
+      ],
+    },
+  ],
+
+  // Second Links Container
+  secondListsContainer: [
+    {
+      icon: 'binary',
+      id: '1',
+      links: [
+        {
+          name: 'Spotify',
+          link: 'https://www.spotify.com',
+        },
+        {
+          name: 'Reddit',
+          link: 'https://www.reddit.com',
+        },
+        {
+          name: 'Hashnode',
+          link: 'https://www.hashnode.com',
+        },
+        {
+          name: 'Pocket',
+          link: 'https://www.pocket.com',
+        },
+      ],
+    },
+    {
+      icon: 'github',
+      id: '2',
+      links: [
+        {
+          name: 'Front',
+          link: 'https://www.reddit.com/r/Frontend/',
+        },
+        {
+          name: 'Rust',
+          link: 'https://www.reddit.com/r/rust/',
+        },
+        {
+          name: 'Go',
+          link: 'https://www.reddit.com/r/golang/',
+        },
+        {
+          name: 'Repos',
+          link: 'https://github.com/migueravila',
+        },
+      ],
+    },
+  ],
+};
